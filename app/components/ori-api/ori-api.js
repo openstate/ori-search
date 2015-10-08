8e450bbc99f4b86690d87610e793893a0fa0fa28
Breyten Ernsting <bje@dds.nl>
2015-10-08 14:16:55 +0200

    Small fixes.

--- app/components/ori-api/ori-api.js
+++ app/components/ori-api/ori-api.js
@@ -1,6 +1,6 @@
 angular.module('oriApp').factory("ORIAPIService", ['$http', function ($http) {
   var svc = {};
-  var base_url = 'http://localhost:5000/v0';
+  var base_url = 'http://api.openraadsinformatie.nl/v0';
 
   svc.simple_search = function(q) {
     return $http.get(base_url + "/search?query=" + q);
