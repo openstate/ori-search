6e774af03b12a13cfb81ad045c458f3e0d39e7df
Breyten Ernsting <bje@dds.nl>
2015-10-13 16:03:16 +0200

    More proper version of 404 controller.

--- app/app.js
+++ app/app.js
@@ -3,31 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('oriApp', [
   'ngRoute',
+  'oriApp.viernulvier',
   'oriApp.search',
   'oriApp.version'
 ]).
-controller('vierNulVierCtrl', ['$scope', '$location',
-		function ($scope, $location) {
-			// QueryService.clearQuery();
-			// QueryService.clearFilterOptions();
-			$scope.usedPage = false;
-
-			var path = 	$location.path().split("/");
-			var pageLoc = path.indexOf('page');
-
-			if(pageLoc > -1){
-				$scope.usedPage = true;
-				path.splice(pageLoc, 2);
-				$scope.newUrl = '#' + path.join('/');
-			}
-}]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when(
-    '/', {redirectTo: '/search'}).
-  otherwise(
-    {
-      templateUrl: 'viernulvier/viernulvier.html',
-      controller: 'vierNulVierCtrl'
-    }
-  );
+  $routeProvider.when('/', {redirectTo: '/search'});
 }]);
---
+++ app/viernulvier/viernulvier.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('oriApp.viernulvier', ['ngRoute']).
+
+controller('vierNulVierCtrl', ['$scope', '$location',
+		function ($scope, $location) {
+			// QueryService.clearQuery();
+			// QueryService.clearFilterOptions();
+			$scope.usedPage = false;
+
+			var path = 	$location.path().split("/");
+			var pageLoc = path.indexOf('page');
+
+			if(pageLoc > -1){
+				$scope.usedPage = true;
+				path.splice(pageLoc, 2);
+				$scope.newUrl = '#' + path.join('/');
+			}
+}]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise(
+    {
+      templateUrl: 'viernulvier/viernulvier.html',
+      controller: 'vierNulVierCtrl'
+    }
+  );
+}]);
