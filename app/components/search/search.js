4e4f3a38b2ee06313ae8b400e66a0736090007eb
Breyten Ernsting <bje@dds.nl>
2015-10-20 12:03:43 +0200

    Fix display of events date

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -71,15 +71,6 @@ angular.module('oriApp.search', ['ngRoute'])
   };
 })
 
-.filter("from_iso8601", function () {
-  return function (val) {
-    if (val) {
-      var info = val.split('T');
-      return info[0] + ' ' + info[1];
-    }
-  };
-})
-
 .filter('unsafe', function($sce) {
   return function(val) {
     return $sce.trustAsHtml(val);
@@ -90,21 +81,7 @@ angular.module('oriApp.search', ['ngRoute'])
   return function(val) {
     if (val) {
       var tijd_datum = new Date(val);
-      console.log('de tijd voor ' + val);
-      console.dir(tijd_datum);
-      var dag = tijd_datum.getDay(); //dag in woorden
-      var dag2 = tijd_datum.getDate(); // dag in getal
-      var maand = tijd_datum.getMonth(); // +1 want js begint bij 0 te tellen
-      var jaar = tijd_datum.getFullYear();
-
-      var uur = tijd_datum.getHours();
-      var minuten = tijd_datum.getMinutes();
-      var seconden = tijd_datum.getSeconds();
-
-      var maandarray = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-      var dagarray = new Array('zondag','maandag','dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
-
-      return dagarray[dag]+" "+dag2+" "+maandarray[maand]+" "+jaar+" "+tijd_datum.toLocaleTimeString('nl-NL');
+      return tijd_datum.toLocaleString();
     }
   };
 })
