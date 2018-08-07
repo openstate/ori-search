cdb157dfdde9687775f973b896048177d2c43f37
Breyten Ernsting <bje@dds.nl>
2018-08-07 12:11:05 +0200

    Add a show more and show less button for large events. Fixes #24

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -303,6 +303,58 @@ angular.module('oriApp.search', ['ngRoute', 'chart.js', 'daterangepicker'])
   };
 })
 
+.directive('showMore', function() {
+  return {
+        restrict: 'A',
+        transclude: true,
+        template: [
+            '<div class="show-more-container"><ng-transclude></ng-transclude></div>',
+            '<a href="#" class="btn btn-default show-more-expand">Toon meer <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></a>',
+            '<a href="#" class="btn btn-default show-more-collapse">Toon minder <span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span></a>',
+        ].join(''),
+        link: function(scope, element, attrs, controller) {
+            var maxHeight = 45;
+            var initialized = null;
+            var containerDom = element.children()[0];
+            var $showMore = angular.element(element.children()[1]);
+            var $showLess = angular.element(element.children()[2]);
+
+            scope.$watch(function () {
+                // Watch for any change in the innerHTML. The container may start off empty or small,
+                // and then grow as data is added.
+                return containerDom.innerHTML;
+            }, function () {
+                if (null !== initialized) {
+                    // This collapse has already been initialized.
+                    return;
+                }
+
+                if (containerDom.clientHeight <= maxHeight) {
+                    // Don't initialize collapse unless the content container is too tall.
+                    return;
+                }
+
+                $showMore.on('click', function () {
+                    element.removeClass('show-more-collapsed');
+                    element.addClass('show-more-expanded');
+                    containerDom.style.height = 'auto'; //null;
+                    return false;
+                });
+
+                $showLess.on('click', function () {
+                    element.removeClass('show-more-expanded');
+                    element.addClass('show-more-collapsed');
+                    containerDom.style.height = maxHeight + 'px';
+                    return false;
+                });
+
+                initialized = true;
+                $showLess.triggerHandler('click');
+            });
+        },
+  };
+})
+
 .factory("SearchService", ['ORIAPIService', 'ConstantsService', 'OptionsService', '$q',
 function (ORIAPIService, ConstantsService, OptionsService, $q) {
   var svc = {};
