3cb309fde37b1d23630a655b37e3107760d76ecd
Breyten Ernsting <bje@dds.nl>
2018-09-20 17:57:43 +0200

    Prevent showing double URLs. Fixes #35

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -66,6 +66,47 @@ var defered_resolver = {
 
 angular.module('oriApp.search', ['ngRoute', 'chart.js', 'daterangepicker'])
 
+.filter('unique', function () {
+
+  // from https://github.com/angular-ui/angular-ui-OLDREPO/blob/master/modules/filters/unique/unique.js
+  
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var hashCheck = {}, newItems = [];
+
+      var extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  };
+})
+
 .factory('httpInterceptor', ['$q', '$rootScope',
     function ($q, $rootScope) {
         var loadingCount = 0;
